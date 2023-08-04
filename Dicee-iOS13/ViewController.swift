265a53fd9996f0febde6bc63b45368f7a1534832
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-08-04 15:17:10 -0500

    Randomized rolls

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,20 +13,15 @@ class ViewController: UIViewController {
 	@IBOutlet weak var diceImageView1: UIImageView!
 	@IBOutlet weak var diceImageView2: UIImageView!
 	
-	var leftDiceNumber = 1
-	
-	override func viewDidLoad() {
-        super.viewDidLoad()
-        
-		
-	}
-	
 	@IBAction func rollButtonPressed(_ sender: UIButton) {
-		diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][leftDiceNumber]
-		leftDiceNumber = leftDiceNumber + 1
+		
+		let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+		
+		diceImageView1.image = diceArray[Int.random(in: 0...5)]
+		
+		diceImageView2.image = diceArray.randomElement()
+		
 		
 	}
-	
-
 }
 
